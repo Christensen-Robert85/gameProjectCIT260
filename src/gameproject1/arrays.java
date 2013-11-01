178349d2f23c33b20365d030b366eaa8fd17b13f
Robert <Robert@Robert-PC.PK5001Z>
2013-10-31 23:37:59 -0600

    Lesson 5 commit

---
+++ src/gameproject1/arrays.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameproject1;
+
+/**
+ *
+ * @author Robert
+ */
+public class arrays {
+    public static void main(String[] args) { 
+            
+       int[] array;
+       int intArray[] = {1, 12, 4, 3, 7, 6, 14};
+       int sum = 0;
+      
+       
+       
+   
+    System.out.println("The value is " + sum + ".");  
+    }
+    }
+    
+
